a478ec0632a7c07c8424378230a3e4ab6ededbd3
Mike Fuller <85436507+michaeljosephfuller@users.noreply.github.com>
2025-08-10 00:31:58 +0100

    Re-formatted abstract distribution docstrings

--- src/sdatools/distributions/abstract/discrete_distribution.py
+++ src/sdatools/distributions/abstract/discrete_distribution.py
@@ -8,22 +8,25 @@ class DiscreteDistribution(Distribution):
     """
     Abstract base class for continuous probability distributions.
         
-    Subclasses must implement the following methods / properties:
-        - domain: Domain of the distribution.
-        - mean: Mean of the distribution.
-        - variance: Variance of the distribution.
-        - skewness: Skewness of the distribution.
-        - kurtosis: Excess kurtosis of the distribution.
-        - pmf(x): Probability mass function.
-        - cdf(x): Cumulative distribution function.
-        - __repr__: String representation of the distribution.
-        - __hash__: Hash representation of the distribution.
+    Subclasses must implement:
+    --------------------------
+    - domain          : Domain of the distribution.
+    - mean            : Mean of the distribution.
+    - variance        : Variance of the distribution.
+    - skewness        : Skewness of the distribution.
+    - kurtosis        : Excess kurtosis of the distribution.
+    - pmf(x)          : Probability mass function.
+    - cdf(x)          : Cumulative distribution function.
+    - sample(size=1)  : Generate n (n=size) samples from the distribution.
+    - __repr__        : String representation of the distribution.
+    - __hash__        : Hash representation of the distribution.
 
-    Methods / properties provided:
-        - stddev: Standard deviation of the distribution.
-        - sample(size=1): Generate n (n=size) samples from the distribution.
-        - __str__: String representation (takes value of __repr__ if not provided).
-        - __eq__, __ne__: Check if two distributions are equal / not equal.
+    Provided by base class:
+    -----------------------
+    - stddev          : Standard deviation of the distribution.
+    - __str__         : Shortened string representation (defaults to __repr__).
+    - __eq__          : Check if two distributions are equal
+    - __ne__          : Check if two distributions are not equal.
     """
 
     # Distribution functions
--- src/sdatools/distributions/abstract/distribution.py
+++ src/sdatools/distributions/abstract/distribution.py
@@ -8,21 +8,23 @@ class Distribution(ABC):
     """
     Abstract base class for probability distributions.
 
-    Subclasses must implement the following methods / properties:
-        - domain: Domain of the distribution.
-        - mean: Mean of the distribution.
-        - variance: Variance of the distribution.
-        - skewness: Skewness of the distribution.
-        - kurtosis: Excess kurtosis of the distribution.
-        - sample(size=1): Generate n (n=size) samples from the distribution.
-        - __repr__: String representation of the distribution.
-        - __hash__: Hash representation of the distribution.
+    Subclasses must implement:
+    --------------------------
+    - domain          : Domain of the distribution.
+    - mean            : Mean of the distribution.
+    - variance        : Variance of the distribution.
+    - skewness        : Skewness of the distribution.
+    - kurtosis        : Excess kurtosis of the distribution.
+    - sample(size=1)  : Generate n (n=size) samples from the distribution.
+    - __repr__        : String representation of the distribution.
+    - __hash__        : Hash representation of the distribution.
 
-    Methods / properties provided:
-        - stddev: Standard deviation of the distribution.
-        - __str__: String representation (takes value of __repr__ if not overridden).
-        - __eq__: Check if two distributions are equal
-        - __ne__: Check if two distributions are not equal.
+    Provided by base class:
+    -----------------------
+    - stddev          : Standard deviation of the distribution.
+    - __str__         : Shortened string representation (defaults to __repr__).
+    - __eq__          : Check if two distributions are equal
+    - __ne__          : Check if two distributions are not equal.
     """
 
     # Special methods
